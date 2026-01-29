715086f8c5a263d7c87853d1ad126a28bf823243
shevcodn <shevcodn@proton.me>
2026-01-29 03:32:34 -0500

    p139: solve task

---
+++ phase_03_oop/p139.py
@@ -0,0 +1,14 @@
+class Point:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def __eq__(self, other):
+		return self.x == other.x and self.y == other.y
+
+p1 = Point(3, 5)
+p2 = Point(3, 5)
+p3 = Point(1, 2)
+
+print(p1 == p2)
+print(p1 == p3)
