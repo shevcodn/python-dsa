831c12e897bf8c1c7fe81f5e495dfc2392cfea1c
shevcodn <shevcodn@proton.me>
2026-01-29 03:17:35 -0500

    p135: solve task

---
+++ phase_03_oop/p135.py
@@ -0,0 +1,23 @@
+class Shape:
+	def area(self):
+		pass
+
+class Square(Shape):
+	def __init__(self, side):
+		self.side = side
+
+	def area(self):
+		return self.side * self.side
+
+class Circle(Shape):
+	def __init__(self, radius):
+		self.radius = radius
+
+	def area(self):
+		return 3.14 * self.radius * self.radius
+
+square = Square(5)
+circle = Circle(3)
+
+print(square.area())
+print(circle.area())
