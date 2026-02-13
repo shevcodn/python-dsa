9c096f69a32c327372e2b50e6ade263a01ddc71d
shevcodn <shevcodn@proton.me>
2026-02-13 17:07:47 -0500

    p267: solve task

---
+++ phase_05_data_structures/p267.py
@@ -0,0 +1,12 @@
+def flatten(lst):
+    result = []
+    for item in lst:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+    return result
+
+print(flatten([1, [2, 3], [4, [5, 6]]]))
+print(flatten([1, 2, 3]))
+print(flatten([[1, [2]], [3, [4, [5]]]]))
