37fd77013900143d624cd72347f48af74a55f35f
shevcodn <shevcodn@proton.me>
2026-02-13 15:58:06 -0500

    p256: solve task

---
+++ phase_05_data_structures/p256.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def prepend(self, data):
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+
+    def delete(self, data):
+        current = self.head
+        previous = None
+        while current:
+            if current.data == data:
+                if previous:
+                    previous.next = current.next
+                else:
+                    self.head = current.next
+                return
+            previous = current
+            current = current.next
+
+    def search(self, data):
+        current = self.head
+        while current:
+            if current.data == data:
+                return True
+            current = current.next
+        return False
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.prepend(0)
+ll.display()
+
+print(ll.search(2))
+print(ll.search(9))
+
+
+
