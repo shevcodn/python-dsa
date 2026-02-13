c5d8bcf6abf8836a8737bced92990d17676b4ffa
shevcodn <shevcodn@proton.me>
2026-02-13 16:16:15 -0500

    p257: solve task

---
+++ phase_05_data_structures/p257.py
@@ -0,0 +1,75 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def prepend(self, data):
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+
+    def delete(self, data):
+        current = self.head
+        previous = None
+        while current:
+            if current.data == data:
+                if previous:
+                    previous.next = current.next
+                else:
+                    self.head = current.next
+                return
+            previous = current
+            current = current.next
+
+
+    def length(self):
+        count = 0
+        current = self.head
+        while current:
+            count += 1
+            current = current.next
+        return count
+
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        self.head = prev
+
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+
+print(ll.length())
+
+ll.reverse()
+ll.display()
+
+
