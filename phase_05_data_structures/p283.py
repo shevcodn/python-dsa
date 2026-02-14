652b31095ab717d6a6f385efa51c882411339af5
shevcodn <shevcodn@proton.me>
2026-02-14 09:02:05 -0500

    p283: solve task

---
+++ phase_05_data_structures/p283.py
@@ -0,0 +1,12 @@
+def reverse_string(s):
+    s = list(s)
+    left, right = 0, len(s) - 1
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+    return ''.join(s)
+    
+print(reverse_string("hello"))
+print(reverse_string("abcde"))
+print(reverse_string("a"))
