23acaccf4e9491aed8fe78211be589565f7cf71d
shevcodn <shevcodn@proton.me>
2026-02-14 08:35:00 -0500

    p276: solve task

---
+++ phase_05_data_structures/p276.py
@@ -0,0 +1,11 @@
+def char_frequency(s):
+    frequency = {}
+    for char in s:
+        if char in frequency:
+            frequency[char] += 1
+        else:
+            frequency[char] = 1
+    return frequency
+
+print(char_frequency("hello"))
+print(char_frequency("aabbc"))
