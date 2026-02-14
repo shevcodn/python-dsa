9f46aa8d79cd9139303f50911bdacc6979cd2630
shevcodn <shevcodn@proton.me>
2026-02-14 08:46:35 -0500

    p279: solve task

---
+++ phase_05_data_structures/p279.py
@@ -0,0 +1,12 @@
+def group_anagrams(words):
+    anagram_dict = {}
+    for word in words:
+        sorted_word = ''.join(sorted(word))
+        if sorted_word in anagram_dict:
+            anagram_dict[sorted_word].append(word)
+        else:
+            anagram_dict[sorted_word] = [word]
+    return list(anagram_dict.values())
+    
+result = group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
+print(result)
