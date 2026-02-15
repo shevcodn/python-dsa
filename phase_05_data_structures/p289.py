bd5c77cc74109a6fa4e1af2467832a32813d1cb3
shevcodn <shevcodn@proton.me>
2026-02-15 15:51:18 -0500

    p289: solve task

---
+++ phase_05_data_structures/p289.py
@@ -0,0 +1,15 @@
+def search_insert(nums, target):
+    left, right = 0, len(nums)
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid
+    return left
+    
+print(search_insert([1, 3, 5, 6], 5))
+print(search_insert([1, 3, 5, 6], 2))
+print(search_insert([1, 3, 5, 6], 7))
+print(search_insert([1, 3, 5, 6], 0))
+
