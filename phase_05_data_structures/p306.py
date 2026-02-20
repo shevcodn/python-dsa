d765a825cc0119e6a043511fb5f821ca2c2e5621
shevcodn <shevcodn@proton.me>
2026-02-20 12:40:18 -0500

    p306: solve task

---
+++ phase_05_data_structures/p306.py
@@ -0,0 +1,56 @@
+from collections import deque
+
+class TreeNode:                                                                                                                       
+    def __init__(self, val):
+        self.val = val                                                                                                                
+        self.left = None                                                                                                            
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, val):
+        if not self.root:
+            self.root = TreeNode(val)
+            return
+        current = self.root
+        while True:
+            if val < current.val:
+                if current.left:
+                    current = current.left
+                else:
+                    current.left = TreeNode(val)
+                    break
+            else:
+                if current.right:
+                    current = current.right
+                else:
+                    current.right = TreeNode(val)
+                    break
+
+    def level_order(self, root):
+        if not root:
+            return []
+        result = []
+        queue = deque([root])
+        while queue:
+            level_size = len(queue)
+            level = []
+            for _ in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+        return result
+    
+tree = BST()
+tree.insert(5)
+tree.insert(3)
+tree.insert(7)
+tree.insert(1)
+
+print(tree.level_order(tree.root))
