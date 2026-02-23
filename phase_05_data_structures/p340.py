db8cd6f2fa68aa07ebb9ab864d779de2e5770cdd
shevcodn <shevcodn@proton.me>
2026-02-23 15:29:40 -0500

    p340: solve task

---
+++ phase_05_data_structures/p340.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+def walls_and_gates(rooms):
+    if not rooms:
+        return []
+    
+    rows, cols = len(rooms), len(rooms[0])
+    queue = deque()
+
+    for r in range(rows):
+        for c in range(cols):
+            if rooms[r][c] == 0:
+                queue.append((r, c))
+
+    directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+    while queue:
+        r, c = queue.popleft()
+        for dr, dc in directions:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < rows and 0 <= nc < cols and rooms[nr][nc] == 2147483647:
+                rooms[nr][nc] = rooms[r][c] + 1
+                queue.append((nr, nc))
+    
+    return rooms
+
+INF = 2147483647
+rooms = [
+      [INF, -1,  0, INF],
+      [INF, INF, INF, -1],
+      [INF, -1, INF, -1],
+      [0,  -1, INF, INF]
+  ]
+
+walls_and_gates(rooms)
+print(rooms)
