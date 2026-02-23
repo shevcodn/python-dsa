14e8c224666cd67f2a34d26ee5efeb42e525121e
shevcodn <shevcodn@proton.me>
2026-02-23 18:36:39 -0500

    p365: solve task

---
+++ phase_05_data_structures/p365.py
@@ -0,0 +1,18 @@
+def word_pattern(pattern, s):
+    words = s.split()
+    if len(pattern) != len(words):
+        return False
+    char_to_word = {}
+    word_to_char = {}
+    for c, w in zip(pattern, words):
+        if c in char_to_word and char_to_word[c] != w:
+            return False
+        if w in word_to_char and word_to_char[w] != c:
+            return False
+        char_to_word[c] = w
+        word_to_char[w] = c
+    return True
+
+print(word_pattern("abba", "dog cat cat dog"))
+print(word_pattern("abba", "dog cat cat fish"))
+print(word_pattern("aaaa", "dog cat cat dog"))
