a496a68299f306328dadf166513628698f1b49e6
shevcodn <shevcodn@proton.me>
2026-02-23 17:02:32 -0500

    p355: solve task

---
+++ phase_05_data_structures/p355.py
@@ -0,0 +1,9 @@
+def is_happy(n):
+    seen = set()
+    while n != 1 and n not in seen:
+        seen.add(n)
+        n = sum(int(digit) ** 2 for digit in str(n))
+    return n == 1
+
+print(is_happy(19))
+print(is_happy(2))
