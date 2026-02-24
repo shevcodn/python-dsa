6372fb2c41b2a1a62baf27db3e136ead471ad311
shevcodn <shevcodn@proton.me>
2026-02-24 15:26:04 -0500

    p389: solve task | progress: 389/1000

---
+++ phase_05_data_structures/p389.py
@@ -0,0 +1,14 @@
+def find_peak(nums):
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = left + (right - left) // 2
+        if nums[mid] > nums[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+    return left
+
+
+print(find_peak([1,2,3,1]))
+print(find_peak([1,2,1,3,5,6,4]))
+
