28dd6091d630c26b6034d9b33b25a0a6da30a26a
shevcodn <shevcodn@proton.me>
2026-02-25 15:30:25 -0500

    p390: solve task | progress: 391/1000

---
+++ phase_05_data_structures/p390.py
@@ -0,0 +1,17 @@
+def my_sqrt(x):
+    if x < 2:
+        return x
+    left, right = 1, x // 2
+    while left <= right:
+        mid = (left + right) // 2
+        if mid * mid == x:
+            return mid
+        elif mid * mid < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return right
+
+print(my_sqrt(4))
+print(my_sqrt(8))
+print(my_sqrt(16))
