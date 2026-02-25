c5a024f8a27f93b9e53fbec22606a497623c39ba
shevcodn <shevcodn@proton.me>
2026-02-25 15:53:35 -0500

    p397: solve task | progress: 397/1000

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 ## Progress
 
 ```
-[███████░░░░░░░░░░░░░] 396 / 1000 tasks complete
+[███████░░░░░░░░░░░░░] 397 / 1000 tasks complete
 ```
 
 | Phase | Topics | Tasks | Status |
---
+++ phase_05_data_structures/p397.py
@@ -0,0 +1,16 @@
+def max_profit(prices):
+    if not prices:
+        return 0
+    
+    min_price = float('inf')
+    max_profit = 0
+
+    for price in prices:
+        min_price = min(min_price, price)
+        max_profit = max(max_profit, price - min_price)
+
+    return max_profit
+
+print(max_profit([7,1,5,3,6,4]))
+print(max_profit([7,6,4,3,1]))
+print(max_profit([1,2]))
