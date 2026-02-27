54f8d7696b155f5c21939118c40e25f26a06c6be
shevcodn <shevcodn@proton.me>
2026-02-27 15:16:20 -0500

    p402: solve task | progress: 402/1000

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 ## Progress
 
 ```
-[████████░░░░░░░░░░░░] 401 / 1000 tasks complete
+[████████░░░░░░░░░░░░] 402 / 1000 tasks complete
 ```
 
 | Phase | Topics | Tasks | Status |
---
+++ phase_06_algorithms/p402.py
@@ -0,0 +1,13 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+print(selection_sort([5, 3, 1, 4, 2]))
+print(selection_sort([9, 1, 8, 2, 7]))
+            
