e70eeec6da32417675d7c2a306c5d161b1526dc9
shevcodn <shevcodn@proton.me>
2026-02-27 16:14:46 -0500

    p405: solve task | progress: 405/1000

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 ## Progress
 
 ```
-[████████░░░░░░░░░░░░] 404 / 1000 tasks complete
+[████████░░░░░░░░░░░░] 405 / 1000 tasks complete
 ```
 
 | Phase | Topics | Tasks | Status |
---
+++ phase_06_algorithms/p405.py
@@ -0,0 +1,10 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[-1]
+    left = [x for x in arr[:-1] if x <= pivot]
+    right = [x for x in arr[:-1] if x > pivot]
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+print(quick_sort([5, 3, 1, 4, 2]))
+print(quick_sort([9, 1, 8, 2, 7]))
