d8e29171f077c1081589bdd7745a42fde7b2d54f
shevcodn <shevcodn@proton.me>
2026-03-02 23:43:42 -0500

    p433: solve task | progress: 433/1000

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 ## Progress
 
 ```
-[████████░░░░░░░░░░░░] 432 / 1000 tasks complete
+[████████░░░░░░░░░░░░] 433 / 1000 tasks complete
 ```
 
 | Phase | Topics | Tasks | Status |
---
+++ phase_06_algorithms/p433.py
@@ -0,0 +1,15 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+print(binary_search([1, 3, 5, 7, 9, 11], 7))
+print(binary_search([1, 3, 5, 7, 9, 11], 6))
+print(binary_search([2], 2))
