11c7dddae6efe14cb7ba4aefa0d50eea3612d154
shevcodn <shevcodn@proton.me>
2026-03-04 00:28:55 -0500

    p442: solve task | progress: 442/1000

---
+++ phase_06_algorithms/p442.py
@@ -0,0 +1,17 @@
+def longest_unique(s):
+    char_set = set()
+    left = 0
+    max_len = 0
+
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[right])
+            left = left + 1
+        char_set.add(s[right])
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
+
+print(longest_unique("abcabcbb"))
+print(longest_unique("bbbbb"))
+print(longest_unique("pwwkew"))
