4920cb8da228954f7094bd57eb66a2395a04fd41
shevcodn <shevcodn@proton.me>
2026-03-04 02:58:51 -0500

    p463: solve task | progress: 463/1000

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 ## Progress
 
 ```
-[█████████░░░░░░░░░░░] 462 / 1000 tasks complete
+[█████████░░░░░░░░░░░] 463 / 1000 tasks complete
 ```
 
 | Phase | Topics | Tasks | Status |
---
+++ phase_06_algorithms/p463.py
@@ -0,0 +1,12 @@
+import heapq
+
+def kth_largest(nums, k):
+    heap = []
+    for num in nums:
+        heapq.heappush(heap, num)
+        if len(heap) > k:
+            heapq.heappop(heap)
+    return heap[0]
+
+print(kth_largest([3, 1, 5, 12, 2, 11], 2))
+print(kth_largest([7, 10, 4, 3, 20, 15], 4))
