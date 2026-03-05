436d054192c4fa7dfd9a124e5985df5c3fb040f4
shevcodn <shevcodn@proton.me>
2026-03-05 01:08:54 -0500

    p513: solve task | progress: 513/1000

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 ## Progress
 
 ```
-[██████████░░░░░░░░░░] 512 / 1000 tasks complete
+[██████████░░░░░░░░░░] 513 / 1000 tasks complete
 ```
 
 | Phase | Topics | Tasks | Status |
---
+++ phase_06_algorithms/p513.py
@@ -0,0 +1,16 @@
+def max_profit(prices):
+    min_price = float('inf')
+    max_profit = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        else:
+            max_profit = max(max_profit, price - min_price)
+
+    return max_profit
+
+print(max_profit([7, 1, 5, 3, 6, 4]))
+print(max_profit([7, 6, 4, 3, 1]))
+print(max_profit([2, 4, 1]))
+print(max_profit([1]))
