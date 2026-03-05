f955cacaed7a4bca09f2f7f2539f13a1c884c52c
shevcodn <shevcodn@proton.me>
2026-03-05 01:03:59 -0500

    p511: solve task | progress: 511/1000

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 ## Progress
 
 ```
-[██████████░░░░░░░░░░] 510 / 1000 tasks complete
+[██████████░░░░░░░░░░] 511 / 1000 tasks complete
 ```
 
 | Phase | Topics | Tasks | Status |
---
+++ phase_06_algorithms/p511.py
@@ -0,0 +1,21 @@
+def longest_palindrome_subseq(s):
+    n = len(s)
+    dp = [[0] * n for _ in range(n)]
+
+    for i in range(n):
+        dp[i][i] = 1
+
+    for length in range(2, n + 1):
+        for i in range(n - length + 1):
+            j = i + length - 1
+            if s[i] == s[j]:
+                dp[i][j] = dp[i+1][j-1] + 2
+            else:
+                dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+
+    return dp[0][n-1]
+
+print(longest_palindrome_subseq("bbbab"))
+print(longest_palindrome_subseq("cbbd"))
+print(longest_palindrome_subseq("a"))
+print(longest_palindrome_subseq("aa"))
