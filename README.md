a9117cb7973c95dfda84ed67607a378b0cd23898
shevcodn <shevcodn@proton.me>
2026-03-04 21:59:40 -0500

    p486: solve task | progress: 486/1000

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 ## Progress
 
 ```
-[█████████░░░░░░░░░░░] 485 / 1000 tasks complete
+[█████████░░░░░░░░░░░] 486 / 1000 tasks complete
 ```
 
 | Phase | Topics | Tasks | Status |
---
+++ phase_06_algorithms/p486.py
@@ -0,0 +1,33 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def has_cycle(head):
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            return True
+
+    return False
+
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node2
+
+print(has_cycle(node1))
+
+a = ListNode(1)
+b = ListNode(2)
+a.next = b
+print(has_cycle(a))
