f83998b4478507139d2e5c541b5dc9c3ed6cd45d
shevcodn <shevcodn@proton.me>
2026-03-04 22:44:12 -0500

    p494: solve task | progress: 494/1000

---
+++ phase_06_algorithms/p494.py
@@ -0,0 +1,28 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def lowest_common_ancestor(root, p, q):
+    while root:
+        if p.val < root.val and q.val < root.val:
+            root = root.left
+        elif p.val > root.val and q.val > root.val:
+            root = root.right
+        else:
+            return root
+
+root = TreeNode(6)
+root.left = TreeNode(2)
+root.right = TreeNode(8)
+root.left.left = TreeNode(0)
+root.left.right = TreeNode(4)
+root.left.right.left = TreeNode(7)
+root.right.right = TreeNode(9)
+root.left.right.left = TreeNode(3)
+root.left.right.right = TreeNode(5)
+
+print(lowest_common_ancestor(root, TreeNode(2), TreeNode(8)).val)
+print(lowest_common_ancestor(root, TreeNode(2), TreeNode(4)).val)
+print(lowest_common_ancestor(root, TreeNode(0), TreeNode(5)).val)
