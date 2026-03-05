492de494575b80bffa90583993f8e47a3679f53a
shevcodn <shevcodn@proton.me>
2026-03-05 14:43:22 -0500

    p539: solve task | progress: 539/1000

---
+++ phase_06_algorithms/p539.py
@@ -0,0 +1,22 @@
+def maximalSquare(matrix):
+    if not matrix:
+        return 0
+    
+    m, n = len(matrix), len(matrix[0])
+    dp = [[0] * n for _ in range(m)]
+    max_side = 0
+    for i in range(m):
+        for j in range(n):
+            if matrix[i][j] == '1':
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                max_side = max(max_side, dp[i][j])
+        
+    return max_side ** 2
+
+print(maximalSquare([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]))
+print(maximalSquare([["0","1"],["1","0"]]))
+print(maximalSquare([["1"]]))
+print(maximalSquare([["0"]]))
