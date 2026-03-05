297775b3a0070db1aac0b57c77be2d36dbfdc3c7
shevcodn <shevcodn@proton.me>
2026-03-04 22:48:19 -0500

    p495: solve task | progress: 495/1000

---
+++ phase_06_algorithms/p495.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def right_side_view(root):
+    if not root:
+        return []
+
+    result = []
+    queue = deque([root])
+
+    while queue:
+        level_size = len(queue)
+        
+        for i in range(level_size):
+            node = queue.popleft()
+
+            if i == level_size - 1:
+                result.append(node.val)
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+    return result
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.right = TreeNode(5)
+root.right.right = TreeNode(4)
+
+print(right_side_view(root))         # [1, 3, 4]
+print(right_side_view(TreeNode(1)))  # [1]
+print(right_side_view(None))         # []
